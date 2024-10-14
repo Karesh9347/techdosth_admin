79e6e21cafe42f61aae2898e6859bcf0792a38d7
pattapu naresh <161163501+Karesh9347@users.noreply.github.com>
2024-10-14 09:52:10 +0530

    Update Urls.jsx

--- src/Urls.jsx
+++ src/Urls.jsx
@@ -4,15 +4,21 @@ import AddContest from './AddContest'
 import {BrowserRouter,Route,Routes} from 'react-router-dom'
 import AdminSidebar from './AdminSidebar'
 import AddAptitude from './AddAptitude'
+import AddInterview from './AddInterview'
+import AddQuery from './AddQuery'
+import AddProblem from './AddProblem'
 const Urls = () => {
   return (
     <div>
         <BrowserRouter>
         <Routes>
         <Route path="/" element={<AdminSidebar/>}/>
+        <Route path="/add-problem" element={<AddProblem/>}/>
             <Route path="/add-question" element={<AddQuestion/>}/>
+            <Route path="/add-query" element={<AddQuery/>}/>
             <Route path="/add-contest" element={<AddContest/>}/>
             <Route path="/add-aptitude" element={<AddAptitude/>}/>
+            <Route path="/add-interview" element={<AddInterview/>}/>
         </Routes>
         </BrowserRouter>
     </div>
