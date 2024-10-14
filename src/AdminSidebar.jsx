7333b97c841deabc5a2fdef282e57cd5861cb656
pattapu naresh <161163501+Karesh9347@users.noreply.github.com>
2024-10-14 09:54:34 +0530

    Update AdminSidebar.jsx

--- src/AdminSidebar.jsx
+++ src/AdminSidebar.jsx
@@ -9,7 +9,11 @@ const AdminSidebar = () => {
       <Nav.Link as={Link} to="/add-question" className="nav-link">Add DSA Question</Nav.Link>
       <Nav.Link as={Link}  to="/add-contest" className="nav-link">Add Contest</Nav.Link>
       <Nav.Link as={Link}  to="/add-aptitude" className="nav-link">Add Aptitude Question</Nav.Link>
+      <Nav.Link as={Link}  to="/add-interview" className="nav-link">Add interview Question</Nav.Link>
+      <Nav.Link as={Link}  to="/add-query" className="nav-link">Add QUERY OF THE DAY</Nav.Link>
+      <Nav.Link as={Link}  to="/add-problem" className="nav-link">Add problem OF THE DAY</Nav.Link>
     </Nav>
+  
   );
 };
 
